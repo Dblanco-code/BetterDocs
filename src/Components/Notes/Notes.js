1121dff57cf12e765ddb2a09632a2c00addaab96
dbrahmbh <dbrahmbh@nd.edu>
2023-07-01 17:15:46 -0400

    fixing async notes

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Parse from 'parse';
-import NotesList from './NotesList';
-import getNote from "../../Services/Notes"
 
-const Notes = () => {
+import {createNote, getAllNotes, removeNote} from "../../Services/Notes"
+import NotesList from './NotesList';
+
+/* const Notes = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
@@ -75,7 +76,106 @@ const Notes = () => {
       </div>
     </section>
   );
-};
+}; */
+
+const Notes = () => {
+  // Variables in the state to hold data
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState([]);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // UseEffect to run when the page loads to
+  // obtain async data and render
+  useEffect(() => {
+    getAllNotes().then((notes) => {
+      console.log(notes);
+      setNotes(notes);
+    });
+  }, []);
+
+  // Flags in the state to watch for add/remove updates
+  const [add, setAdd] = useState(false);
+  const [remove, setRemove] = useState('');
+  
+  useEffect(() => {
+    // Check for add flag and make sure title and content state variables are defined
+    if (title && content && add) {
+      createNote(title, content).then((newNote) => {
+        setAdd(false);
+        // Add the newly created note to the notes array
+        // to render the new list of notes (thru spread/concatination)
+        setLessons([...notes, newNote]);
+      });
+    }
+
+    // Check if remove state variable is holding an ID
+    if (remove.length > 0) {
+      //Filter the old notes list to take out selected note
+      const newNotes = notes.filter((note) => note.id !== remove);
+      setNotes(newNotes);
+
+      removeNote(remove).then(() => {
+        console.log("Removed note with ID: ", remove);
+      });
+      // Reset remove state variable
+      setRemove("");
+    }
+  }, [title, content, notes, add, remove]);
+
+  // Handler to handle event passed from child submit button
+  const onClickHandler = (event) => {
+    event.preventDefault();
+    // Trigger add flag to create note and
+    // re-render list with new note
+    setAdd(true);
+  };
+
+    // Handler to track changes to the title input text
+    const onTitleChangeHandler = (event) => {
+      event.preventDefault();
+      console.log(event.target.value);
+      // Continuously updating title to be added on submit
+      setTitle(event.target.value);
+    };
+
+    // Handler to track changes to the title input text
+    const onContentChangeHandler = (event) => {
+      event.preventDefault();
+      console.log(event.target.value);
+      // Continuously updating title to be added on submit
+      setContent(event.target.value);
+    };
+
+    return (
+      <section>
+        <div className="container">
+          <h2>Make a Note</h2>
+          <div>
+            <div>
+              <input
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={handleTitleChange}
+              />
+            </div>
+            <div>
+              <textarea
+                placeholder="Content"
+                value={content}
+                onChange={handleContentChange}
+              ></textarea>
+            </div>
+            <button onClick={handleSubmit}>Submit</button>
+          </div>
+          {successMessage && <p className="success-message">{successMessage}</p>}
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </div>
+      </section>
+    );
+}
 
 export default Notes;
-
--- src/Services/Notes.js
+++ src/Services/Notes.js
@@ -1,6 +1,40 @@
 import Parse from 'parse';
+/* SERVICE FOR PARSE SERVER OPERATIONS */
 
-// READ operation - get info about note from Parse Note class
+// CREATE operation - new note with title and content
+export const createNote = (title, content) => {
+  console.log("Creating note with title: ", title);
+  const Note = Parse.Object.extend("Note");
+  const note = new Note();
+  // using setter to UPDATE the object
+  note.set("Title", title);
+  note.set("Content", content)
+  return note.save().then((result) => {
+    // returns new Note object
+    return result;
+  });
+};
+
+// READ operation - get all notes in Parse class Note
+export const getAllNotes = () => {
+  const Note = Parse.Object.extend("Note");
+  const query = new Parse.Query(Note);
+  return query.find().then((results) => {
+    // returns array of Note objects
+    return results;
+  });
+};
+
+// DELETE operation - remove note by ID
+export const removeNote = (id) => {
+  const Note = Parse.Object.extend("Note");
+  const query = new Parse.Query(Note);
+  return query.get(id).then((note) => {
+    note.destroy();
+  });
+};
+
+/* //READ operation - get info about note from Parse class Note
 const getNote = async() => {
     const Note = Parse.Object.extend('Note');
     const query = new Parse.Query(Note);
@@ -14,6 +48,4 @@ const getNote = async() => {
     catch (error) {
       console.error('Error while fetching Note', error);
     }
-}
-
-export default getNote;
+} */
