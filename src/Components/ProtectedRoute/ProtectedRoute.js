ba7011d896fd28805e837d21574da20fcf0ac326
dbrahmbh <dbrahmbh@nd.edu>
2023-07-19 23:39:22 -0400

    fixed another smalle issue

--- src/Components/ProtectedRoute/ProtectedRoute.js
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { checkUser } from "../../Common/Services/AuthService";
-import "../../CSS/protectedRoute.css";
+import "../../Css/protectedRoute.css";
 
 const ProtectedRoute = ({ element: Component, ...rest }) => {
   console.log("element: ", Component);
